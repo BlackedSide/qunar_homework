400bf073255d8bba6c90de8354a58c699bb22b26
Black Funny <184131195@qq.com>
2021-07-09 11:37:43 +0800

    Http Socket modified
    修改了socket服务端请求http数据的代码，依然使用传统IO方式。

--- src/question_5/SocketClientMock.java
+++ src/question_5/SocketClientMock.java
@@ -1,20 +1,25 @@
 package question_5;
 
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class SocketClientMock {
+    private static final int port = 8888;
+
     public static void main(String[] args) throws Exception {
         InetAddress host = InetAddress.getLocalHost();
         Scanner in = new Scanner(System.in);
         while (in.hasNextLine()) {
-            Socket socket = new Socket(host.getHostName(), 8888);
+            Socket socket = new Socket(host.getHostName(), port);
             String message = in.nextLine();
-            if (message.equalsIgnoreCase("exit")) break;
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
             oos.writeObject(message);
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            String response = (String) ois.readObject();
+            System.out.println(response);
         }
     }
 }
--- src/question_5/SocketServerMock.java
+++ src/question_5/SocketServerMock.java
@@ -1,9 +1,10 @@
 package question_5;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -30,22 +31,51 @@ public class SocketServerMock {
     }
 
     private static final int port = 8888;
+    private static final String USER_AGENT = "Mozilla/5.0";
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         ServerSocket sever = new ServerSocket(port);
         while (true) {
             Socket socket = sever.accept();
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-            String message = (String) ois.readObject();
-            if (message.equalsIgnoreCase("exit")) break;
-            System.out.println("汉字个数为： " + countChinese(message));
-            System.out.println("字母个数为： " + countLetter(message));
-            System.out.println("标点个数为： " + countMark(message));
-            System.out.println("总字符数为： " + message.length());
+            String url = (String) ois.readObject();
+
+            /*
+              请求 http 并解析响应体
+             */
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", USER_AGENT);
+            int responseCode = con.getResponseCode();
+            if (responseCode == 200) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+//                System.out.println(response.toString());
+                System.out.println("汉字个数为： " + countChinese(response.toString()));
+                System.out.println("字母个数为： " + countLetter(response.toString()));
+                System.out.println("标点个数为： " + countMark(response.toString()));
+                System.out.println("总字符数为： " + response.length());
+                ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                oos.writeObject(
+                        "汉字个数为： " + countChinese(response.toString()) + "\n"
+                                + "字母个数为： " + countLetter(response.toString()) + "\n"
+                                + "标点个数为： " + countMark(response.toString()) + "\n"
+                                + "总字符数为： " + response.length()
+                );
+                in.close();
+            } else {
+//                System.out.println(responseCode);
+                System.out.println("请求http数据时发生了错误……");
+                ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                oos.writeObject("请求http数据时发生了错误，请检查……");
+            }
             ois.close();
             socket.close();
         }
-        System.out.println("Bye");
-        sever.close();
     }
 }
